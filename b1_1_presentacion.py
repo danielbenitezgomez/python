b5679d9b7428d2e76520de7d7974eceb4a97b388
Daniel Benítez <broeg33@gmail.com>
2025-10-17 13:17:36 +0200

    Delete b1_1_presentacion.py

--- b1_1_presentacion.py
+++
@@ -1,4 +0,0 @@
-print("Hola bro, ¿Que dise?")
-nombre = input("¿Por cierto, como te llamas? ")
-edad = input("¿Y cuantos años tienes? ")
-print(f"Por lo visto te llamas {nombre} y tienes {edad} años")
