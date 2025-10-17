b4f76aab2d70f71b13e67832a3dcc001215124a8
Daniel Benítez <broeg33@gmail.com>
2025-10-17 13:17:57 +0200

    Delete b1_3_conversor_datos.py

--- b1_3_conversor_datos.py
+++
@@ -1,9 +0,0 @@
-nombre = input("¿Como te llamas? ")
-edad_persona = input("¿Cuantos años tienes? ")
-altura_persona = input("¿Y cuanto mides? ")
-
-edad = int(edad_persona)
-altura = float(altura_persona)
-
-
-print(f"Hola, me llamo {nombre}, tengo {edad} años y mido {altura} metros")
